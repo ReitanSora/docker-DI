0ffe855069b64819c7f4edb581521e26016f79cf
Stiven <stivenpilca@gmail.com>
2025-05-09 20:02:48 -0500

    build: add middleware to know private ip

--- docker-frontend/src/App.tsx
+++ docker-frontend/src/App.tsx
@@ -10,32 +10,38 @@ interface UserProps {
 
 function App() {
   const [users, setUsers] = useState<[]>();
-  const [ip, setIp] = useState();
+  const [privateIp, setPrivateIp] = useState('');
+  const [publicIp, setPublicIp] = useState('');
   const tunnelBaseUrl = import.meta.env.VITE_TUNNEL_BASE_URL;
-  const hostIP = window.location.hostname
-  // const localBaseUrl = import.meta.env.VITE_LOCAL_BASE_URL;
+  const baseUrl = import.meta.env.VITE_BASE_URL;
 
   useEffect(() => {
     const getUsers = async () => {
       try {
         const res = await fetch(`${tunnelBaseUrl}/users`)
         const response = await res.json();
-        setIp(response.ip)
         setUsers(response);
       } catch {
         try {
-          const res = await fetch(`http://${hostIP}:5001/users`)
+          const res = await fetch(`http://${baseUrl}:5001/users`)
           const response = await res.json();
-          setIp(response.ip)
           setUsers(response);
         } catch (error) {
           console.log(error)
         }
+      } finally {
+        const privateIpRes = await fetch('/api/ip')
+        const privateIp = await privateIpRes.json();
+        setPrivateIp(privateIp.ip)
+
+        const publicIpRes = await fetch('https://api.ipify.org?format=json')
+        const publicIp = await publicIpRes.json();
+        setPublicIp(publicIp.ip)
       }
     }
 
     getUsers();
-  }, [hostIP, tunnelBaseUrl])
+  }, [baseUrl, tunnelBaseUrl])
 
   return (
     <>
@@ -49,11 +55,9 @@ function App() {
       </div>
       <h2>Vite + React</h2>
       <h2>Practica Docker</h2>
-      <h2>Ip privada: {hostIP}</h2>
-      {ip && <h2>Ip pública: {ip}</h2>}
       <div className="card">
         <div>
-          <p>Usuarios registrados:</p>
+          <h2>Usuarios registrados:</h2>
           {users && (
             users.map((item: UserProps, index) => {
               return (
@@ -66,6 +70,9 @@ function App() {
             })
           )}
         </div>
+        <h2>IP:</h2>
+        <p>Privada: {privateIp}</p>
+        <p>Pública: {publicIp}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- docker-frontend/vite.config.ts
+++ docker-frontend/vite.config.ts
@@ -1,13 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { networkInterfaces } from 'node:os';
+
+const getLocalIP = () => {
+  const nets = networkInterfaces();
+  const results = {};
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        if (!results[name]) results[name] = [];
+        results[name].push(net.address);
+      }
+    }
+  }
+
+  // Priorizar direcciones comunes de LAN
+  const priority = ['en0', 'eth0', 'wlan0'];
+  for (const iface of priority) {
+    if (results[iface]) return results[iface][0];
+    
+  }
+  console.log(results.Ethernet)
+  return results.Ethernet[0] || 'localhost';
+};
+
+const myPlugin = () => ({
+  name: 'configure-server',
+  configureServer(server) {
+    server.middlewares.use('/api/ip', (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ ip: getLocalIP() }));
+    });
+  }
+})
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), myPlugin()],
   server: {
     host: true,
     strictPort: true,
     port: 5173,
-    // allowedHosts: ['jan-some-cams-got.trycloudflare.com']
   },
 })
